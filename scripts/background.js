6dffe998315c842e204400690147f5472320289c
Krishna Noutiyal <krishnanoutiyal@gmail.com>
2025-06-05 09:19:01 +0530

    Ceating Core and Background JS scripts

---
+++ scripts/background.js
@@ -0,0 +1,7 @@
+// background.js
+// This file is required for the extension's service worker (background tasks).
+// Currently, no background logic is needed, but this file is necessary for manifest v3.
+
+chrome.runtime.onInstalled.addListener(() => {
+  // Extension installed or updated
+});
---
+++ scripts/core.js
@@ -0,0 +1,14 @@
+console.log("Copy-Paste ITR Running");
+
+document.body.onselectstart = null;
+document.onselectstart = null;
+
+document.body.oncopy = null;
+document.body.oncut = null;
+
+document.body.onpaste = null;
+
+document.oncontextmenu = null;
+document.body.oncontextmenu = null;
+
+console.log("Copy-Paste ITR Script Execution Successfully Complete.");
