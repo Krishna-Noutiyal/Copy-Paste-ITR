7af95cfb694bac0b5cd5e29576a0dd722612ce1a
Krishna Noutiyal <krishnanoutiyal@gmail.com>
2025-06-05 09:38:38 +0530

    Added a popup screen and modified the core.js

--- scripts/core.js
+++ scripts/core.js
@@ -1,5 +1,31 @@
 console.log("Copy-Paste ITR Running");
 
+// Enables copy-paste functionality on *.incometax.gov.in
+(function enableCopyPaste() {
+  const events = [
+    'copy', 'cut', 'paste',
+    'contextmenu', 'keydown', 'keypress', 'keyup'
+  ];
+
+  function removeBlockers(element) {
+    events.forEach(event => {
+      element[`on${event}`] = null;
+    });
+  }
+
+  function processAll() {
+    const elements = document.querySelectorAll('input, textarea, [contenteditable="true"]');
+    elements.forEach(removeBlockers);
+  }
+
+  // Initial run
+  processAll();
+
+  // Also run when new elements are added
+  const observer = new MutationObserver(processAll);
+  observer.observe(document.body, { childList: true, subtree: true });
+})();
+
 document.body.onselectstart = null;
 document.onselectstart = null;
 
